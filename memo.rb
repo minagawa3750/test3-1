73f0bd1d938c8c28a5a70dc86b4e49294b296332
EC2 Default User <ec2-user@ip-172-31-43-251.us-west-2.compute.internal>
2022-07-10 15:06:14 +0000

    修正_メモアプリ課題

--- memo.rb
+++ memo.rb
@@ -1,32 +1,50 @@
 require "csv"
- 
+
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
  
 memo_type = gets.to_i
 
 if memo_type == 1
-  puts "拡張子を除いたファイルを入力してください"
+ CSV.open("memo.csv", "w") do |test|
+   test << [memo_type]
+ end
+ 
+ puts "拡張子を除いたファイルを入力してください"
+ 
   file_type = gets.chomp
+  
+  CSV.open("memo.csv", "a") do |test|
+   test << [file_type]
+ end
+ 
   puts "メモしたい内容を入力してください"
   puts "完了したらctl+Dをおします"
+  
   memo_text = readlines
   
-  CSV.open("memo.csv", "w") do |test|
-   test << [memo_type]
-   test << [file_type]
+  CSV.open("memo.csv", "a") do |test|
    test << [memo_text]
   end
   
   
 elsif memo_type == 2
+  CSV.open("text.csv", "w") do |test|
+   test << [memo_type]
+  end
+  
+  puts "拡張子を除いたファイルを入力してください"
+  
   file_type = gets.chomp
+  
+  CSV.open("text.csv", "a") do |test|
+   test << [file_type]
+  end
+  
   puts "編集したいメモの内容を入力してください"
   puts "完了したらctl+Dをおします"
   memo_text = readlines
   
    CSV.open("text.csv", "a") do |test|
-   test << [memo_type]
-   test << [file_type]
    test << [memo_text]
   end
   
